e996587055c4d02d33a7f18dca25750b8994bc48
FelipeSysten <76599815+FelipeSysten@users.noreply.github.com>
2024-05-26 08:44:01 -0300

    contador e excepção

---
+++ src/fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package fluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -6086626396261815127L;
+
+	public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
